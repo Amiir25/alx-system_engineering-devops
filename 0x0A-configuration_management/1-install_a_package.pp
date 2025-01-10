ceb25adfc5f6ae7591023c5071b83244db47fdd0
Amiir25 <amirsadik636@gmail.com>
2025-01-10 15:27:59 +0300

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -6,7 +6,7 @@ package { 'python3-pip':
 
 exec { 'install_flask':
     command => '/usr/bin/pip3 install flask==2.1.0',
-    path    => ['usr/bin', '/bin'],
+    path    => ['/usr/bin', '/bin'],
     unless  => '/usr/bin/pip3 show flask | grep -q "Version: 2.1.0"',
     require => Package['python3-pip'],
 }
