ffad29402a51e15b26900e777c7bc8fb3eb953b0
Amiir25 <amirsadik636@gmail.com>
2025-01-10 15:45:03 +0300

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,11 @@
 # Installs Flask from pip3
 
+package { 'python3-pip':
+    ensure => installed,
+}
+
 package { 'Flask':
     ensure   => '2.1.0',
     provider => 'pip3',
+    require  => Package['python3-pip'],
 }
