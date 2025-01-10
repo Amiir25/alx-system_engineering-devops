d9e29fde60e83534240371ccc85831e93e9ac5e3
Amiir25 <amirsadik636@gmail.com>
2025-01-10 14:09:32 +0300

    Add 1-install_a_package.pp

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,12 @@
+# Installs Flask from pip3
+
+package { 'python3-pip':
+    ensure => installed,
+}
+
+exe { 'install_flask':
+    command => '/usr/bin/pip3 install flask==2.1.0',
+    path    => ['usr/bin', '/bin'],
+    unless  => '/usr/bin/pip3 show flask | grep -q "Version: 2.1.0"',
+    require => Package['python3-pip'],
+}
