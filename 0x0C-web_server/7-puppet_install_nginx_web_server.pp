d1f949aa298f551077376fd2d4918f95c9b800da
Amiir25 <144411155+Amiir25@users.noreply.github.com>
2025-01-30 23:19:45 +0300

    Update 7-puppet_install_nginx_web_server.pp

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,1 +1,37 @@
->>>
+class nginx_setup {
+    package { 'nginx':
+        ensure => installed,
+    }
+
+    service { 'nginx':
+        ensure    => running,
+        enable    => true,
+        require   => Package['nginx'],
+    }
+
+    file { '/var/www/html/index.html':
+        ensure  => file,
+        content => '<html><body><h1>Hello World!</h1></body></html>',
+        require => Package['nginx'],
+    }
+
+    file { '/etc/nginx/sites-available/default':
+        ensure  => file,
+        content => "
+server {
+    listen 80;
+    root /var/www/html;
+    index index.html;
+
+    location / {
+        try_files \$uri \$uri/ =404;
+    }
+
+    location /redirect_me {
+        return 301 http://example.com;
+    }
+}",
+        require => Package['nginx'],
+        notify  => Service['nginx'],
+    }
+}
