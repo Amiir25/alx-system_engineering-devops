2b2d1e894bf742bdb4d8e31e9ede0f9cded8162a
Amiir25 <144411155+Amiir25@users.noreply.github.com>
2025-01-30 23:27:36 +0300

    Update 7-puppet_install_nginx_web_server.pp

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,40 +1,54 @@
 #!/usr/bin/enc bash
 # Puppet script to install Nginx web server
 
-class nginx_setup {
-    package { 'nginx':
-        ensure => installed,
-    }
+# Install the Nginx package
+package { 'nginx':
+  ensure => installed,
+}
 
-    service { 'nginx':
-        ensure  => running,
-        enable  => true,
-        require => Package['nginx'],
-    }
+# Ensure Nginx service is running and enabled
+service { 'nginx':
+  ensure     => running,
+  enable     => true,
+  require    => Package['nginx'],
+}
 
-    file { '/var/www/html/index.html':
-        ensure  => file,
-        content => '<html><body><h1>Hello World!</h1></body></html>',
-        require => Package['nginx'],
-    }
-
-    file { '/etc/nginx/sites-available/default':
-        ensure  => file,
-        content => "
+# Create the Nginx configuration file
+file { '/etc/nginx/sites-available/default':
+  ensure  => file,
+  content => '
 server {
-    listen 80;
+    listen 80 default_server;
+    listen [::]:80 default_server;
+
+    server_name _;
+
     root /var/www/html;
-    index index.html;
+    index index.html index.htm;
 
     location / {
-        try_files \$uri \$uri/ =404;
+        return 200 "Hello World!";
     }
 
     location /redirect_me {
-        return 301 http://example.com;
-    }
-}",
-        require => Package['nginx'],
-        notify  => Service['nginx'],
+        return 301 http://$host/;
     }
 }
+',
+  require => Package['nginx'],
+  notify  => Service['nginx'],
+}
+
+# Ensure the configuration directory exists
+file { '/var/www/html':
+  ensure => directory,
+  require => Package['nginx'],
+}
+
+# Ensure the index.html file exists and contains "Hello World!"
+file { '/var/www/html/index.html':
+  ensure  => file,
+  content => 'Hello World!',
+  require => File['/var/www/html'],
+}
+
