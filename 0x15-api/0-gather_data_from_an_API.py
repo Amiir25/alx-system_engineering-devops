c4bb278b753ba47659f4ee61a59676815b4446d9
Amiir25 <amirsadik636@gmail.com>
2025-02-10 20:39:15 +0300

    Update 0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -2,7 +2,7 @@
 '''
 0-gather_data_from_an_api.py
 
-Gather ingormation about TODO List progress of a user
+Gather information about TODO List progress of a user
 '''
 
 import requests
@@ -18,33 +18,35 @@ if __name__ == "__main__":
     USER_DATA_API = "https://jsonplaceholder.typicode.com/users"
     TODO_LIST_API = "https://jsonplaceholder.typicode.com/todos"
 
-    id = sys.argv[1]
+    id = int(sys.argv[1])
 
     # GET request to fetch user data
-    response = requests.get(f"{USER_DATA_API}/{id}")
+    user_response = requests.get(f"{USER_DATA_API}/{id}")
 
-    if response.status_code != 200:
+    if user_response.status_code != 200:
         print(f"Error: Status code {response.status_code}")
+        exit(1)
     else:
-        user_data = response.json()
+        user_data = user_response.json()
 
     # Extract the name of the user
     user_name = user_data["name"]
 
     # GET request to fetch TODO List
-    response = requests.get(f"{TODO_LIST_API}?userID={id}")
+    todo_response = requests.get(f"{TODO_LIST_API}?userId={id}")
 
-    if response.status_code != 200:
+    if todo_response.status_code != 200:
         print(f"Error: Status code {response.status_code}")
+        exit(1)
     else:
-        todo_list = response.json()
+        todo_list = todo_response.json()
 
     # Extract data from the TODO List
     TOTAL_NUMBER_OF_TASKS = len(todo_list)
-    NUMBER_OF_DONE_TASKS = sum(1 for task in todo_list if task["completed"])
     TASK_TITLE = [task["title"] for task in todo_list if task["completed"]]
+    NUMBER_OF_DONE_TASKS = len(TASK_TITLE)
 
     # Print
     print(f"Employee {user_name} is done with tasks {NUMBER_OF_DONE_TASKS}/{TOTAL_NUMBER_OF_TASKS}:")
-    for i in range(NUMBER_OF_DONE_TASKS):
-        print(f"\t {TASK_TITLE}")
+    for task in TASK_TITLE:
+        print(f"\t {task}")
