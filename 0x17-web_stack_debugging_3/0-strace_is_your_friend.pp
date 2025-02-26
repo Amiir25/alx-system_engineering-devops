c1e526eb6501ea45f299874f21f49e4aa795983b
Amiir25 <144411155+Amiir25@users.noreply.github.com>
2025-02-26 17:40:14 +0300

    Add 0-strace_is_your_friend.pp

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,32 @@
+class apache_fix {
+
+  # Ensure correct ownership and permissions
+  file { '/var/www/html':
+    ensure  => directory,
+    owner   => 'www-data',
+    group   => 'www-data',
+    mode    => '0755',
+    recurse => true,
+  }
+
+  # Ensure Apache is installed
+  package { 'apache2':
+    ensure => installed,
+  }
+
+  # Ensure required modules are enabled
+  exec { 'enable_php_module':
+    command => '/usr/sbin/a2enmod php',
+    unless  => '/bin/ls /etc/apache2/mods-enabled/php*',
+    notify  => Service['apache2'],
+  }
+
+  # Ensure Apache service is running and enabled
+  service { 'apache2':
+    ensure    => running,
+    enable    => true,
+    require   => Package['apache2'],
+  }
+}
+
+include apache_fix
